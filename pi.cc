a04af296be4b00e53fd7b4e3cc740f880462174c
Ashroom66 <ashroom09@gmail.com>
2022-09-22 14:15:31 +0900

    feat: モンテカルロでπを近似(動かし続ける)

--- pi.cc
+++ pi.cc
@@ -5,14 +5,17 @@
 #include <stdlib.h>
 
 int main() {
-    int N = 10000000;
+    int runs = 0;
     int inside_circle = 0;
     double r = 1;
-    for (int i=0; i<N; i++) {
+    std::cout << std::fixed << std::setprecision(12);                           // 表示する桁数, set::fixedとセットで小数部分の桁数を指定
+    while (true) {
+        runs ++;
         auto x = random_double(-r, r);
         auto y = random_double(-r, r);
         if (x*x + y*y < r*r) {inside_circle++;}
+        if (runs % 100000 == 0) {
+            std::cout << "Estimate of Pi = " << 4*double(inside_circle) / runs << "\t runs=" << runs << '\n';    // 10億あたりから3.1415でほぼ確定。次の桁はオーバーフローして収束せず
+        }
     }
-    std::cout << std::fixed << std::setprecision(12);                           // 表示する桁数, set::fixedとセットで小数部分の桁数を指定
-    std::cout << "Estimate of Pi = " << 4*double(inside_circle) / N << '\n';    // Estimate of Pi = 3.088000000000 (N=1000)
 }
