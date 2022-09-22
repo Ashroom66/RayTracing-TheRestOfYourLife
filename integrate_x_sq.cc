ee2cd0d8a111d3fc8de2d8d4850a353f59ad13cb
Ashroom66 <ashroom09@gmail.com>
2022-09-22 17:20:17 +0900

    feat: 一様乱数で再びx^2の積分を推定

--- integrate_x_sq.cc
+++ integrate_x_sq.cc
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 inline double pdf(double x) {
-    return 0.5 * x;
+    return 0.5;
 }
 
 inline double f(double x) {
@@ -17,10 +17,10 @@ int main() {
     int N = 1000000;
     auto sum = 0.0;
     for (int i=0; i<N; i++) {
-        // auto x = random_double(0, 2);                // 一様乱数。効率悪い
-        auto x = sqrt(random_double(0, 4));             // 工夫した乱数。一様乱数より効率よくなる
-        while (x==0.0) {x = sqrt(random_double(0, 4));} // (zero divisionを回避)
-        sum += f(x) / pdf(x);                           // 一様乱数じゃないので、サンプルされる確率から重みづけする
+        auto x = random_double(0, 2);                   // 一様乱数。効率悪い
+        // auto x = sqrt(random_double(0, 4));          // 工夫した乱数。一様乱数より効率よくなる(pdfはx/2になる)
+        while (x==0.0) {x = sqrt(random_double(0, 2));} // (zero divisionを回避)
+        sum += f(x) / pdf(x);                           // サンプルされる確率(pdf)で重みづけ
     }
     std::cout << std::fixed << std::setprecision(12);
     std::cout << "I = " << sum/N << '\n';               // 一様サンプリングと異なり、ここで関数の範囲を気にする必要が無くなる
